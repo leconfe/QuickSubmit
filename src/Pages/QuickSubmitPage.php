fe1ddda8cdd2ab80eda55172384dcd79b2d1e2aa
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-12-10 09:43:49 +0800

    chore: add compatibility for Leconfe 1.3.3

--- src/Pages/QuickSubmitPage.php
+++ src/Pages/QuickSubmitPage.php
@@ -19,7 +19,7 @@ use Filament\Forms\Components\Livewire;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use App\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
